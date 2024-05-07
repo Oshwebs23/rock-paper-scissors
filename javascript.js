54b4643656b0d705d5f9acc903ed5477058620c1
Oshwebs23 <osian1996@live.co.uk>
2024-05-07 17:33:58 +0100

    final step completed. Function created to play 5 rounds of game

--- javascript.js
+++ javascript.js
@@ -1,17 +1,51 @@
 function getComputerChoice () {
-    let get = Math.floor(Math.random() * 3)
-    if (get == 1) {
-        return "Rock";
-    } else if (get == 2) {
-        return "Paper";
+    let computerChoice = Math.floor(Math.random() * 3)
+    if (computerChoice == 1) {
+        return "rock";
+    } else if (computerChoice == 2) {
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
  }
-getComputerChoice();
 
 function getHumanChoice () {
-    let yourChoice = prompt ("Please choose Rock, Paper or Scissors");
+    let humanChoice = prompt ("Please choose Rock, Paper or Scissors");
+    return humanChoice.toLowerCase();
 }
 
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0; 
+
+/* logic to play a round of the game */
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("It's a draw!");
+    } else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+              (humanChoice == "paper" && computerChoice == "rock") ||
+              (humanChoice == "scissors" && computerChoice == "paper")) {
+            humanScore++;
+            console.log(`Well done you have won! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore++;
+        console.log(`You have lost! ${computerChoice} beats ${humanChoice}`);
+    }
+}
+
+const humanSelection = getHumanChoice ();
+const computerSelection = getComputerChoice ();
+
+/* logic to play 5 rounds of the game */
+
+function playGame () {
+    for (i=1; i<5; i++) {
+        const humanSelection = getHumanChoice ();
+        const computerChoice = getComputerChoice ();
+        playRound (humanSelection, computerSelection);
+        
+    }
+
+}
+
+playGame(); 
